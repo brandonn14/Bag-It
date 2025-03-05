870c2dbfe2dce733b7359ddeb40015a77ed08dcd
mattwyand <194294135+mattwyand@users.noreply.github.com>
2025-03-05 15:04:24 -0500

    add comments for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 
+// -- Page imports --
 import TestPage from "./pages/TestPage";
 import HomePage from "./pages/HomePage";
+
+
+// -- Main page manager for the whole site --
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
+				{/* Link URLS to jsx pages here */}
 				<Route path="/" element={<HomePage />} />
 				<Route path="/test" element={<TestPage />} />
 			</Routes>
